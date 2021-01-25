69b63fbcd6ba6e53163689288ba194b888d815cf
Mamata4198 <76000247+Mamata4198@users.noreply.github.com>
2021-01-25 10:21:37 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 //import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, HashRouter, Switch, Route } from "react-router-dom"
-import page1 from "./Component/page1"
-import page2 from "./Component/page2"
+import parent from "./components/parent"
+
 
 function App() {
   return (
     <Router>
       <HashRouter>
         <Switch>
-          <Route path="/" exact component={page1} />
-          <Route path="/page2" component={page2} />
+          <Route path="/" exact component={parent} />
+    {/* <Route path="/page2" component={page2} />*/}
          
           {/* <Route component={Error} /> */}
         </Switch>
